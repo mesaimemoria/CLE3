24923e4151ca09f0e11f9825295ddd661faae203
unknown <mesaimemoria@gmail.com>
2020-12-17 23:49:21 +0800

    indexphp

---
+++ index.php
@@ -0,0 +1,71 @@
+<html>
+	<head>
+		<link rel="stylesheet" href="styles.css">
+	</head>
+	<body>
+		<ul class="navbar">
+		  <li class="navbaritem"><a href="./css/home.html">ABOUT ME</a></li>
+		  <li class="navbaritem"><a href="./canvas/home.html">CANVAS</a></li>
+		  <li class="navbaritem"><a href="./svg/home.html">SVG</a></li>
+		  <li class="navbaritem"><a href="./css/home.html">CSS</a></li>
+		  <li class="navbaritem" style="float:left">
+			<div class="s">S</div>
+			<div class="circle"></div>
+			<div class="circle1"></div>
+			<div class="circle2"></div>
+		  </li>
+		</ul>
+		
+		<br />
+		
+		<div class="animated-title">
+		  <div class="text-top">
+			<div>
+			  <span>welcome</span>
+			  <span>to</span>
+			</div>
+		  </div>
+		  <div class="text-bottom">
+			<div>sai's studio</div>
+		  </div>
+		</div>
+		
+		<div class="gaming">Welcome to my<br />
+			<div style="color: #fd5492;">3rd Year 1st Semester</div>
+			Portfolio
+			<br /><br />
+			By <br /> 
+			<div style="color: #fd5492;">Mesai Memoria</div>
+			Class ITL303_C
+			<br /><br />
+			<div class="message">WANT TO SEE MORE? SEE MY ACCOUNTS BELOW</div>
+			
+		</div>
+		
+		<div class="footer">
+		    <div class="bg"></div>
+			<div class="bg bg2"></div>
+			<div class="bg bg3"></div>
+			<div class="social">
+				<div class="bb" style="padding-right: 50px;">
+					<a href="https://www.facebook.com/saigraphicsstudio">
+					<img style="width: 30px; margin-bottom: 5px;" src="facebook.png">
+					<br />saigraphicsstudio
+					</a>
+				</div>
+				<div class="bb" style="padding-right: 50px;">
+					<a href="https://www.instagram.com/saigraphix/">
+					<img style="width: 30px; margin-bottom: 5px;" src="instagram.png">
+					<br />saigraphix
+					</a>
+				</div>
+				<div class="bb">
+					<a href="https://www.patreon.com/saisstudio">
+					<img style="width: 30px; margin-bottom: 5px;" src="patreon.png">
+					<br />saisstudio
+					</a>
+				</div>
+			</div>
+		</div>
+	</body>
+</html>
